a321c374bdfb99039d6105050268ced9005ac1ce
Apeirophobia <ervinpusijainen@gmail.com>
2026-03-25 11:34:44 +0200

    Refactor: Migrations

---
+++ Opimerchant/Migrations/20260325092823_CreatedSubFolders.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Opimerchant.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatedSubFolders : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
