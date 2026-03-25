d7efabf9645eaeeb7813917530b50244f43d2a92
Apeirophobia <ervinpusijainen@gmail.com>
2026-03-25 11:34:08 +0200

    Refactor: DbContext and SubFolders
    Created DbContext -> OpimerchantDbContext
    
    Updated Project.cs
    
    Created SubFolders:
    Data
    Core
    Migrations

---
+++ Opimerchant/Data/OpimerchantDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Opimerchant.Models;
+
+namespace Opimerchant.Data
+{
+    public class OpimerchantDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Opimerchant;Trusted_Connection=True;");
+        }
+    }
+}
--- Opimerchant/Program.cs
+++ Opimerchant/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Opimerchant.Data;
+
 namespace Opimerchant
 {
     public class Program
@@ -8,6 +11,8 @@ namespace Opimerchant
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<OpimerchantDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
